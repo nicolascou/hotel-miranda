7c22d5d50e87dd2fd1c8f194c48d60dd20d3159f
nicolascou <nicolascousillas1@gmail.com>
2023-04-09 16:48:31 +0200

    add styles for rooms

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,6 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js';
 
-const swiper = new Swiper('.swiper', {
+new Swiper('.rooms__swiper', {
   direction: 'horizontal',
   autoplay: {
     delay: 5000
